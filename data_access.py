27061d7b17bdaa3a460726ae28b77046f6407695
joshvernon <jvernon_483@hotmail.com>
2017-09-09 21:26:43 -0500

    WIP changes to data_access

--- data_access.py
+++ data_access.py
@@ -1,6 +1,6 @@
 # data_access.py
 
-def sum():
+def sum(table_name='expenses'):
     # Sum stuff.
 
 def insert():
@@ -8,3 +8,14 @@ def insert():
 
 def create():
     # Create stuff.
+
+def scrub_table_name(table_name):
+    # Provides sanitization of input table names in SQL queries,
+    # since SQLite doesn't allow query parameterization of table names.
+    # Shamelessly stolen from here:
+    # http://stackoverflow.com/questions/3247183/variable-table-name-in-sqlite
+    allowed_chrs = ['_']
+    return ''.join(
+        chr for chr in table_name
+        if (chr.isalnum()) or (chr in allowed_chrs)
+    )
