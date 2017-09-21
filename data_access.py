bf745b6ab691279519b97b13d91603f03f17c85f
joshvernon <jvernon_483@hotmail.com>
2017-09-20 23:48:09 -0500

    Implement data_access.insert()

--- data_access.py
+++ data_access.py
@@ -5,35 +5,6 @@ import sqlite3
 DATABASE = 'expenses.db'
 TABLE_NAME = 'expenses'
 
-class DataAccess():
-
-    def __init__(self, database=DATABASE, table_name=TABLE_NAME):
-        self.database = database
-        self.table_name = table_name
-        self.connection = _get_connection(database)
-
-    def __repr__(self):
-        return ("DataAccess(database:'{0}', table_name:'{1}')"
-                .format(self.database, self.table_name))
-
-    def close(self):
-        self.connection.close()
-
-    def create(self):
-        create_stmt = "CREATE TABLE IF NOT EXISTS {0}(\
-            description TEXT,\
-            amount REAL,\
-            file_path TEXT,\
-            date TEXT)".format(_scrub_table_name(self.table_name))
-        with self.connection as conn:
-            conn.execute(create_stmt)
-
-    def insert(self):
-        print('Dummy insert.')
-    
-    def sum_expenses(self):
-        print('Dummy sum_expenses.')
-
 def _get_connection(database):
     return sqlite3.connect(database)
 
@@ -47,3 +18,52 @@ def _scrub_table_name(table_name):
         chr for chr in table_name
         if (chr.isalnum()) or (chr in allowed_chrs)
     )
+
+class DataAccess():
+
+    def __init__(self, database=DATABASE, table_name=TABLE_NAME):
+        self.database = database
+        self.table_name = table_name
+        self.connection = _get_connection(database)
+
+    def __repr__(self):
+        return ("DataAccess(database:'{0}', table_name:'{1}')"
+                .format(self.database, self.table_name))
+
+    def _table_exists(self, table_name):
+        select_stmt = "SELECT count(*) FROM sqlite_master\
+            WHERE type = 'table' AND name = ?"
+        cursor = self.connection.execute(select_stmt, (table_name,))
+        result = cursor.fetchone()[0]
+        return False if result == 0 else True
+
+    def close(self):
+        self.connection.close()
+
+    def create(self):
+        create_stmt = "CREATE TABLE IF NOT EXISTS {0}(\
+            description TEXT,\
+            amount REAL,\
+            file_path TEXT,\
+            date TEXT)".format(_scrub_table_name(self.table_name))
+        with self.connection as conn:
+            conn.execute(create_stmt)
+
+    def insert(self, **kwargs):
+        if not kwargs:
+            raise InvalidDataError("Can't insert an empty row")
+        if not self._table_exists(self.table_name):
+            self.create()
+        insert_stmt = "INSERT INTO {0} VALUES(\
+            :description,\
+            :amount,\
+            :file_path,\
+            datetime(:date))".format(_scrub_table_name(self.table_name))
+        with self.connection as conn:
+            conn.execute(insert_stmt, kwargs)
+    
+    def sum_expenses(self):
+        print('Dummy sum_expenses.')
+
+class InvalidDataError(Exception):
+    pass
--- tests.py
+++ tests.py
@@ -1,25 +1,55 @@
 import os
 import sqlite3
 import unittest
+from datetime import datetime
 
-from data_access import DataAccess
+from data_access import DataAccess, InvalidDataError
 
-test_db = 'test.db'
+TEST_DB = 'test.db'
+
+TEST_ROW_1 = {
+    'description': 'description1',
+    'amount': 12.75,
+    'file_path': '/some/file/path',
+    'date': datetime.now(),
+}
+
+TEST_ROW_2 = {
+    'description': 'description2',
+    'amount': 14.24,
+    'file_path': '/some/other/path',
+    'date': datetime.now(),
+}
+
+TEST_ROW_3 = {
+    'description': 'description3',
+    'amount': 5.96,
+    'file_path': '/yet/another/path',
+    'date': datetime.now(),
+    'other_field': 'other_value'
+}
 
 def _table_exists():
-    conn = sqlite3.connect(test_db)
-    c = conn.cursor()
-    c.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='expenses'")
+    conn = sqlite3.connect(TEST_DB)
+    c = conn.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='expenses'")
     result = c.fetchone()[0]
     c.close()
+    conn.close()
+    return result
+
+def _get_row_count():
+    conn = sqlite3.connect(TEST_DB)
+    cur = conn.execute("SELECT count(rowid) from expenses")
+    result = cur.fetchone()[0]
+    cur.close()
     conn.close()
     return result
 
 class DataAccessTestCase(unittest.TestCase):
 
     def tearDown(self):
-        if os.path.exists(test_db):
-            os.remove(test_db)
+        if os.path.exists(TEST_DB):
+            os.remove(TEST_DB)
 
     def test_constructor_with_default_params(self):
         da = DataAccess()
@@ -28,17 +58,39 @@ class DataAccessTestCase(unittest.TestCase):
         da.close()
 
     def test_create_when_table_does_not_exist(self):
-        da = DataAccess(database=test_db)
+        da = DataAccess(database=TEST_DB)
         da.create()
         da.close()
         self.assertEqual(_table_exists(), 1)
     
     def test_create_when_table_already_exists(self):
-        da = DataAccess(database=test_db)
+        da = DataAccess(database=TEST_DB)
         da.create()
         da.create()
         da.close()
         self.assertEqual(_table_exists(), 1)
+    
+    def test_insert_adding_one_row(self):
+        da = DataAccess(database=TEST_DB)
+        da.insert(**TEST_ROW_1)
+        da.close()
+        self.assertEqual(_get_row_count(), 1)
+
+    def test_insert_adding_two_rows(self):
+        da = DataAccess(database=TEST_DB)
+        da.insert(**TEST_ROW_1)
+        da.insert(**TEST_ROW_2)
+        da.close()
+        self.assertEqual(_get_row_count(), 2)
+
+    def test_insert_extra_fields(self):
+        da = DataAccess(database=TEST_DB)
+        da.insert(**TEST_ROW_3)
+        self.assertEqual(_get_row_count(), 1)
+    
+    def test_insert_no_kwargs_raises_InvalidDataError(self):
+        da = DataAccess(database=TEST_DB)
+        self.assertRaises(InvalidDataError, da.insert)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
