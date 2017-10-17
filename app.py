8294b04172c95c68e304ef272171e7b963fa67dd
joshvernon <jvernon_483@hotmail.com>
2017-10-16 22:27:30 -0500

    Add more stuff to Add New Expense, and add padding to both windows

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 import tkinter as tk
+from tkinter import ttk
 
 from data_access import DataAccess
 
@@ -15,14 +16,17 @@ def _format_sum_string(sum_value):
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        super().__init__(master, padx=75, pady=50)
         self.sum = _get_sum()
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
         self.add_new_expense = tk.Button(
-            self, text='Add new expense', command=self.create_add_window
+            self,
+            text='Add new expense',
+            command=self.create_add_window,
+            pady=15,
         )
         self.add_new_expense.pack(side='top')
 
@@ -32,16 +36,17 @@ class Application(tk.Frame):
         self.expenses_label['textvariable'] = self.label_text
         self.expenses_label.pack()
 
-        self.quit = tk.Button(self, text="Quit", command=root.destroy)
-        self.quit.pack(side="bottom")
-
     def create_add_window(self):
-        self.add_window = tk.Toplevel(root)
+        self.add_window = tk.Toplevel(root, padx=35, pady=40)
         self.add_window.title('Add New Expense')
         self.amount_label = tk.Label(self.add_window, text='Expense Amount: $')
         self.amount_label.grid(column=1, row=1)
         self.amount_entry = tk.Entry(self.add_window)
         self.amount_entry.grid(column=2, row=1)
+        self.description_label = tk.Label(self.add_window, text='Description: ')
+        self.description_label.grid(column=1, row=2)
+        self.description_entry = tk.Entry(self.add_window)
+        self.description_entry.grid(column=2, row=2)
 
 root = tk.Tk()
 app = Application(master=root)
