d97731a1a96a90350d881d976c7dd5762c93c571
joshvernon <jvernon_483@hotmail.com>
2017-10-21 17:08:58 -0500

    Create "Add" Button and convert most widgets to local instead of class-level attributes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 import tkinter as tk
 from tkinter import filedialog
 
@@ -18,54 +18,59 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master, padx=75, pady=50)
         self.sum = _get_sum()
+        self.receipt = 'dummy_path'
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.add_new_expense = tk.Button(
+        add_new_expense = tk.Button(
             self,
             text='Add new expense',
             command=self.create_add_window,
             pady=15,
         )
-        self.add_new_expense.pack(side='top')
+        add_new_expense.pack(side='top')
 
-        self.expenses_label = tk.Label(self)
+        expenses_label = tk.Label(self)
         self.label_text = tk.StringVar()
         self.label_text.set(_format_sum_string(self.sum))
-        self.expenses_label['textvariable'] = self.label_text
-        self.expenses_label.pack()
+        expenses_label['textvariable'] = self.label_text
+        expenses_label.pack()
 
     def create_add_window(self):
-        self.add_window = tk.Toplevel(root, padx=35, pady=40)
-        self.add_window.title('Add New Expense')
+        add_window = tk.Toplevel(root, padx=35, pady=40)
+        add_window.title('Add New Expense')
 
-        self.description_label = tk.Label(self.add_window, text='Description: ')
-        self.description_label.grid(column=1, row=1)
-        self.description_entry = tk.Entry(self.add_window)
-        self.description_entry.grid(column=2, row=1)
+        description_label = tk.Label(add_window, text='Description: ')
+        description_label.grid(column=1, row=1)
+        description_entry = tk.Entry(add_window)
+        description_entry.grid(column=2, row=1)
 
-        self.amount_label = tk.Label(self.add_window, text='Amount: $')
-        self.amount_label.grid(column=1, row=2)
-        self.amount_entry = tk.Entry(self.add_window)
-        self.amount_entry.grid(column=2, row=2)
+        amount_label = tk.Label(add_window, text='Amount: $')
+        amount_label.grid(column=1, row=2)
+        amount_entry = tk.Entry(add_window)
+        amount_entry.grid(column=2, row=2)
 
-        self.date_label = tk.Label(self.add_window, text='Date: ')
-        self.date_label.grid(column=1, row=3)
-        self.date_entry = tk.Entry(self.add_window)
-        self.date_entry.grid(column=2, row=3)
+        date_label = tk.Label(add_window, text='Date: ')
+        date_label.grid(column=1, row=3)
+        date_entry = tk.Entry(add_window)
+        date_entry.grid(column=2, row=3)
 
-        self.receipt_label = tk.Label(self.add_window, text='Receipt: ')
-        self.receipt_label.grid(column=1, row=4)
-        self.receipt_button = tk.Button(self.add_window, text='Choose file', command=self.launch_file_dialog)
-        self.receipt_button.grid(column=2, row=4)
+        receipt_label = tk.Label(add_window, text='Receipt: ')
+        receipt_label.grid(column=1, row=4)
+        receipt_button = tk.Button(add_window, text='Choose file', command=self.launch_file_dialog)
+        receipt_button.grid(column=2, row=4)
+
+        add_button = tk.Button(add_window, text='Add', pady=10, padx=25)
+        add_button.grid(row=5, columnspan=5)
 
     def launch_file_dialog(self):
-        filedialog.askopenfilename(
+        file_path = filedialog.askopenfilename(
             initialdir=os.path.expanduser('~/Documents'),
             title="Select file",
-            filetypes=(("PDF files","*.pdf"),("all files","*.*"))
+            filetypes=(("PDF files", "*.pdf"), ("all files", "*.*"))
         )
+        self.receipt = os.path.basename(file_path)
 
 root = tk.Tk()
 app = Application(master=root)
